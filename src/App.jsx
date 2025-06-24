d7607cc2d0e8d7634cdb23304d183205c5db1f6c
getuar <getuarosmani91@gmail.com>
2025-06-24 11:39:07 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import StopWatch from "./componets/stopwatch/StopWatch"
+
+function App() {
+
+    return (
+        <>
+            <div className="container">
+                <StopWatch />
+            </div>
+        </>
+    )
+}
+
+export default App
